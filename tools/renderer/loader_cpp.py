a91cd7c6f3abc8cbd3ed69ee4051f9dad719a9bd
kbm <kabelmann@yahoo.de>
2026-07-17 20:26:20 +0200

    Update Loader cpp.py

--- tools/renderer/loader_cpp.py
+++ tools/renderer/loader_cpp.py
@@ -11,3 +11,19 @@ def render(module, output: Path):
 
         for fn in module.functions:
             f.write(f"PFN_{fn.name} p{fn.name} = nullptr;\n")
+        f.write("\n")
+        f.write("bool LoadOriginalDll()\n")
+        f.write("{\n")
+        f.write('    g_OriginalDll = LoadLibraryW(L"ftd2xx1.dll");\n\n')
+        f.write("    if (!g_OriginalDll)\n")
+        f.write("        return false;\n\n")
+        f.write("    return true;\n")
+        
+        for fn in module.functions:
+            f.write(
+                f'    p{fn.name} = (PFN_{fn.name})GetProcAddress(g_OriginalDll, "{fn.name}");\n'
+            )
+
+        f.write("\n")
+        f.write("    return true;\n")
+        f.write("}\n")
